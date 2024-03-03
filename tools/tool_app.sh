dc16497e905022dc78502d10ff86c9cbabf96726
Pun <45277089+redsunday@users.noreply.github.com>
2024-03-03 23:41:01 +0700

    Update tool_app.sh

--- tools/tool_app.sh
+++ tools/tool_app.sh
@@ -32,6 +32,10 @@ wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.
 dpkg -i /google-chrome-stable_current_amd64.deb
 rm -f /google-chrome-stable_current_amd64.deb
 
+echo "Install Firefox"
+apt-get install firefox-esr -y
+apt-get clean -y
+
 echo "Install noVNC"
 mkdir -p /app/noVNC/utils/websockify
 wget -qO- https://github.com/novnc/noVNC/archive/refs/tags/v1.4.0.tar.gz | tar xz --strip 1 -C /app/noVNC
